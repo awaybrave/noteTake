920d3ba8dd9aee2aef3698ee26d92e72e5063905
awaybrave <awaybrave@gmail.com>
2013-12-21 11:55:16 +0800

    add mouse event to network

--- js/background.js
+++ js/background.js
@@ -113,6 +113,42 @@ var backgroundView = function(){
 		"nodes"	:[],
 		"links" :[]
 	};
+	var _link;
+	var _node;
+	var color  = d3.scale.category20();
+
+	function showAjacentNodes(d, i){
+		if(d.type != 0)
+			return;
+		if(_link){
+			_link.style("stroke", function(l){
+				if(l.target === d)
+					return "#555";
+				else
+					return "#ddd";
+			})
+			.style("stroke-opacity", function(l){
+				if(l.target === d)
+					return 1;
+				else
+					return 0.6; 
+			});
+		}
+	}
+
+	function hideAjacentNodes(d, i){
+		if(d.type != 0)
+			return;
+		if(_link){
+			_link.style("stroke", function(l){
+				return "#ddd";
+			})
+			.style("stroke-opacity", function(l){
+				return 0.6; 
+			});
+		} 
+	}
+
 	var that = {};
 
 	that.addItemToView = function(key, note){
@@ -171,7 +207,6 @@ var backgroundView = function(){
 				//receive an empty object, then start to paint;
 				var width = 600,
 					height = 400;
-				var color  = d3.scale.category20();
 
 				var force = d3.layout.force()
 								.charge(-120)
@@ -184,26 +219,23 @@ var backgroundView = function(){
 				var svg = d3.select("#main-content-network").append("svg")
 							.attr("width", width)
 							.attr("height", height);
-				var link = svg.selectAll(".link")
+				_link = svg.selectAll(".link")
 								.data(_nwjson.links)
 								.enter().append("line")
-								.attr("class", "link")
-								.style("stroke-width", function(d){return Math.sqrt(d.value);});
-				var node = svg.selectAll(".node")
+								.attr("class", "link");
+								//.style("stroke-width", function(d){return Math.sqrt(d.value);});
+				_node = svg.selectAll(".node")
 								.data(_nwjson.nodes)
 								.enter().append("circle")
 								.attr("class", "node")
 								.attr("r", function(d){return d.type == 0 ? 10 : 5;})
 								.style("fill", function(d) {return color(d.group);})
 								.call(force.drag);
-				node.append("title")			
+				_node.append("title")			
 					.text(function(d){if(d.name) return d.name; else return d.id});
 
-				/*
-				node.on("click", function(){
-					alert("hello");
-				});
-				*/
+				
+				_node.on("mouseover", showAjacentNodes).on("mouseout", hideAjacentNodes);
 
 				//drawing the keywords index.
 				var kw = [];
@@ -233,11 +265,11 @@ var backgroundView = function(){
 											.text(function(d) {return d;});
 				
 				force.on("tick", function(){
-					link.attr("x1", function(d){return d.source.x;})		
+					_link.attr("x1", function(d){return d.source.x;})		
 						.attr("y1", function(d){return d.source.y;})	
 						.attr("x2", function(d){return d.target.x;})	
 						.attr("y2", function(d){return d.target.y;})	
-					node.attr("cx", function(d){return d.x;})
+					_node.attr("cx", function(d){return d.x;})
 						.attr("cy", function(d){return d.y;});
 				});
 					}
